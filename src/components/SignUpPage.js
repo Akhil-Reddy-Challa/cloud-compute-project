6896791aacc42911cf55c9ddbe20708e28f14f99
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-03-18 14:47:07 -0400

    designed Sign Up page

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,7 +1,74 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import "../styles/SignUpPage.css";
+import { useHistory } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const SignUpPage = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const history = useHistory();
+  function authenticateUser(event) {
+    event.preventDefault();
+  }
   return (
-    <div>
-      <h3>Sign Up Page</h3>
+    <div className="Login">
+      <Form onSubmit={authenticateUser}>
+        <div>
+          <h2>Sign Up</h2>
+        </div>
+        <Form.Group size="lg">
+          <Form.Control
+            required
+            autoFocus
+            type="text"
+            value={userName}
+            placeholder="User Name"
+            minLength="5"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg">
+          <Form.Control
+            required
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg">
+          <Form.Row>
+            <Col>
+              <Form.Control required placeholder="First name" />
+            </Col>
+            <Col>
+              <Form.Control required placeholder="Last name" />
+            </Col>
+          </Form.Row>
+        </Form.Group>
+        <Form.Group size="lg">
+          <Form.Control
+            required
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <button
+          type="submit"
+          id="loginBtn"
+          disabled={false}
+          className="btn btn-primary btn-lg btn-block"
+        >
+          Sign Up!
+        </button>
+      </Form>
     </div>
   );
 };
