881978c7733cfee194a9739c94effdf97577d0b2
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-03-18 15:02:31 -0400

    HomePage design completed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,26 @@
+import Navbar from "react-bootstrap/Navbar";
+import "../styles/HomePage.css";
+import { useHistory } from "react-router-dom";
+
 const HomePage = (props) => {
+  const history = useHistory();
+  const logOutUser = () => {
+    localStorage.removeItem("user_auth_token");
+    history.push("/login");
+  };
   return (
     <div>
-      <h3>Home Page</h3>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Welcome {props.user_name}</Navbar.Brand>
+        <button
+          id="logOutButton"
+          type="button"
+          className="btn btn-light"
+          onClick={logOutUser}
+        >
+          Log Out!
+        </button>
+      </Navbar>
     </div>
   );
 };
