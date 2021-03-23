564a79db3a82b78188a1bd21076fe06d9dab29b6
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-03-22 23:14:49 -0400

    page1 displays houseHolder's transaction info

--- front-end/src/components/Page1.js
+++ front-end/src/components/Page1.js
@@ -18,60 +18,81 @@ const Page1 = () => {
     if (status === 200) {
       const data = await responseFromServer.json(responseFromServer);
       console.log(data);
+      writeDataToTable(data);
     } else {
       alert("An Error Occured!");
     }
+    // let data = {
+    //   HSHD_NUM: "0011",
+    //   BASKET_NUM: "346420",
+    //   PURCHASE: "13-APR-19",
+    //   PRODUCT_NUM: "5290840",
+    //   DEPARTMENT: "FOOD",
+    // };
+  };
+  const writeDataToTable = (data) => {
+    //Create the table Header's
+    let tableHeader = document.getElementById("tableHeader");
+    let tableRow = document.createElement("tr");
+    for (let heading in data) {
+      let cell = document.createElement("th");
+      let textNode = document.createTextNode(heading);
+      cell.appendChild(textNode);
+      tableRow.appendChild(cell);
+    }
+    tableHeader.appendChild(tableRow);
+
+    //Set the table rows
+    let tableBody = document.getElementById("tableBody");
+    tableRow = document.createElement("tr");
+    for (let heading in data) {
+      let cell = document.createElement("td");
+      let value = data[heading];
+      let textNode = document.createTextNode(value);
+      cell.appendChild(textNode);
+      tableBody.appendChild(cell);
+    }
+    tableBody.appendChild(tableRow);
   };
   return (
-    <div id="mainContainer">
-      <h3>Data-Set "8451_The_Complete_Journey_2_Sample"</h3>
-      <p>
-        Enter the HSHD_NUM below to fetch all the data linked to the number from
-        tables(household, transaction, and products)
-      </p>
-      <form>
-        <div className="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
-          <div className="input-group">
-            <input
-              id="houseHoldNumber"
-              type="search"
-              placeholder="HSHD_NUM"
-              aria-describedby="button-addon1"
-              className="form-control border-0 bg-light"
-            />
-            <div className="input-group-append">
-              <button
-                id="button-addon1"
-                type="submit"
-                className="btn btn-link text-primary"
-                onClick={(e) => fetchRecordsOfCustomer(e)}
-              >
-                <i className="fa fa-search" style={{ fontSize: "18px" }}></i>
-              </button>
+    <div>
+      <div id="mainContainer">
+        <h3>Data-Set "8451_The_Complete_Journey_2_Sample"</h3>
+        <p>
+          Enter the HSHD_NUM below to fetch all the data linked to the number
+          from tables(household, transaction, and products)
+        </p>
+        <form>
+          <div className="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
+            <div className="input-group">
+              <input
+                id="houseHoldNumber"
+                type="search"
+                placeholder="HSHD_NUM"
+                aria-describedby="button-addon1"
+                className="form-control border-0 bg-light"
+              />
+              <div className="input-group-append">
+                <button
+                  id="button-addon1"
+                  type="submit"
+                  className="btn btn-link text-primary"
+                  onClick={(e) => fetchRecordsOfCustomer(e)}
+                >
+                  <i className="fa fa-search" style={{ fontSize: "18px" }}></i>
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-        </table>
+        </form>
       </div>
+      <table
+        id="houseHoldDetails"
+        className="table table-striped table-bordered table-sm"
+      >
+        <thead id="tableHeader" className="table-dark"></thead>
+        <tbody id="tableBody"></tbody>
+      </table>
     </div>
   );
 };
